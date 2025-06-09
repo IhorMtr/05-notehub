dc0277739427e6dbd08448e74b2aff6420ad260d
IhorMtr <igormotornyyy@gmail.com>
2025-06-09 18:34:45 +0300

    init

---
+++ src/components/App/App.tsx
@@ -0,0 +1,8 @@
+import css from './App.module.css';
+import { useEffect, useMemo, useState } from 'react';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import ReactPaginate from 'react-paginate';
+
+export default function App() {
+  return;
+}
